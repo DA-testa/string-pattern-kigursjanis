12f7936f9863968baf4cc06aff6878fb1b9e97f0
kigursjanis <125827153+kigursjanis@users.noreply.github.com>
2023-04-16 22:22:01 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,57 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    
+    input_type = input().strip().upper()
+    
+    if input_type == "I":      
+        pattern = input().strip()
+        text = input().strip()
+        return (input_type , pattern , text)
+        
+    elif input_type == "F":
+        name = 'tests/06'
+        with open(name) as file:
+            pattern = file.readline().strip()
+            text = file.readline().strip()
+            return (input_type , pattern , text)
+    else:
+        print("Invalid input type")
+        exit()
     
     
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
     
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+def get_occurrences(input_type , pattern, text):
+    pattern_len = len(pattern)
+    text_len = len(text)
+    p_hash = sum(ord(pattern[i]) * pow(10, pattern_len - i - 1)for i in range(pattern_len))
+    t_hash = sum(ord(text[i]) * pow(10, pattern_len - i - 1)for i in range(pattern_len))
+    occurrences = []
+    
+    if input_type == "I":
+        for i in range(text_len - pattern_len + 1):
+            if text[i: i + pattern_len] == pattern:
+                occurrences.append(i)
+                
+                
+    elif input_type == "F":
+       
+        for i in range(text_len - pattern_len + 1):
+            if t_hash == p_hash:
+                if text[i: i + pattern_len] == pattern:
+                    occurrences.append(i)
+              
+            if i < text_len - pattern_len:
+                t_hash = (t_hash - ord(text[i]) * pow(10, pattern_len- 1)) *10 + ord(text[i+ pattern_len])
 
-    # and return an iterable variable
-    return [0]
+    return occurrences 
+    
+    
 
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
